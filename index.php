5f4dbddac641d7e69583aad50785d7d965af3198
chaowalit_iig <cwp@ii.co.th>
2021-01-19 18:17:14 +0700

    deploy

--- index.php
+++ index.php
@@ -4,28 +4,3 @@ include_once('google.html');
 
 
 
- $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9kBt168mda_8J9udsdveviGy2Jmw2UJXNicivKpPoDRTtzmVLsL3hBVW1A7reQl1N9zdxMMEHsyhZU7Xp&client_secret=113FEC8F25DF38E3D50707ABBBE9892E302E0FF766AA999038436615B7F5BAA5&username=tsm@ii.co.th.iigscb&password=P@ssw0rd!?johvih6zqLNNmpEJnDQckIks6',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_HTTPHEADER => array(
-            'Cookie: BrowserId=BEkxA0acEeuMRkvPp9yBmg'
-        ),
-    ));
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-    $array = json_decode($response, true);
-    session_start();
-    setcookie( $array['access_token']);
-    echo ($array['access_token']);
-
-
-
---
+++ wsDroplead.php
@@ -0,0 +1,60 @@
+
+
+ if (isset($_POST['form_submitted'])){
+
+  curl_setopt_array($curl, array(
+       CURLOPT_URL => 'https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9kBt168mda_8J9udsdveviGy2Jmw2UJXNicivKpPoDRTtzmVLsL3hBVW1A7reQl1N9zdxMMEHsyhZU7Xp&client_secret=113FEC8F25DF38E3D50707ABBBE9892E302E0FF766AA999038436615B7F5BAA5&username=moo@jan21rs.demo&password=passw0rd2021',
+       CURLOPT_RETURNTRANSFER => true,
+       CURLOPT_ENCODING => '',
+       CURLOPT_MAXREDIRS => 10,
+       CURLOPT_TIMEOUT => 0,
+       CURLOPT_FOLLOWLOCATION => true,
+       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+       CURLOPT_CUSTOMREQUEST => 'POST',
+       CURLOPT_HTTPHEADER => array(
+         'Cookie: BrowserId=BEkxA0acEeuMRkvPp9yBmg'
+       ),
+     ));
+
+     $response = curl_exec($curl);
+     curl_close($curl);
+     $array = json_decode($response, true);
+     session_start();
+     setcookie( $array['access_token']);
+     echo ($array['access_token']);
+
+
+
+     $curlDroplead = curl_init();
+     curl_setopt_arrayDroplead($curlDroplead, array(
+       CURLOPT_URL => 'https://jan21rs-20210113-demo.my.salesforce.com/services/apexrest/wsDroplead/leadProcess',
+       CURLOPT_RETURNTRANSFER => true,
+       CURLOPT_ENCODING => '',
+       CURLOPT_MAXREDIRS => 10,
+       CURLOPT_TIMEOUT => 0,
+       CURLOPT_FOLLOWLOCATION => true,
+       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+       CURLOPT_CUSTOMREQUEST => 'POST',
+       CURLOPT_POSTFIELDS =>'{
+        "reqCreateLead":true,
+        "LeadFirstName":"ทศพล",
+        "LeadLastName":"เกษมสุข"
+     }',
+       CURLOPT_HTTPHEADER => array(
+         'Authorization: Bearer 00D4x000005U3cD!ARgAQM5x22zCQw0zSwl_1mJn6PyPl6UU4.QQ7FOoZEXvtGqjtMQTlBR.yeo3RfSyL3YHEX7zm9p6hXFWqIVPKIh8xOt9Fqc_',
+         'Content-Type: application/json',
+         'Sforce-Auto-Assign: FALSE',
+         'Cookie: BrowserId=BEkxA0acEeuMRkvPp9yBmg'
+       ),
+     ));
+
+     $responseDroplead = curl_exec($curlDroplead);
+     curl_close($curlDroplead);
+     echo $response;
+ }
+
+
+
+
+
+
